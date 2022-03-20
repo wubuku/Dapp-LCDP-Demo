d598bef24c74738b455eea8d5258a254ee256a78
yangjiefeng <yangjiefeng@gmail.com>
2022-03-20 19:09:07 +0800

    add transaction payload encoding method

---
+++ off-chain-service/transactoins/lib.go
@@ -0,0 +1,35 @@
+package transactoins
+
+import diemtypes "github.com/starcoinorg/starcoin-go/types"
+
+func EncodeDomainNameRegisterTxPaylaod(module_addr string,
+	domain_name_id_top_level_domain string,
+	domain_name_id_second_level_domain string,
+	registration_period uint64,
+	smt_root []byte,
+	smt_non_membership_leaf_data []byte,
+	smt_side_nodes []byte,
+) diemtypes.TransactionPayload {
+	module_accont_addr, err := diemtypes.ToAccountAddress(module_addr)
+	if err != nil {
+		panic("module_addr format error: " + module_addr)
+	}
+	return &diemtypes.TransactionPayload__ScriptFunction{
+		Value: diemtypes.ScriptFunction{
+			Module: diemtypes.ModuleId{
+				Address: *module_accont_addr,
+				Name:    diemtypes.Identifier("DomainNameScripts"),
+			},
+			Function: diemtypes.Identifier("register"),
+			TyArgs:   []diemtypes.TypeTag{},
+			Args: [][]byte{
+				encode_u8vector_argument([]byte(domain_name_id_top_level_domain)),    // 	domain_name_id_top_level_domain: vector<u8>,
+				encode_u8vector_argument([]byte(domain_name_id_second_level_domain)), // 	domain_name_id_second_level_domain: vector<u8>,
+				encode_u64_argument(registration_period),                             // 	registration_period: u64,
+				encode_u8vector_argument(smt_root),                                   // 	smt_root: vector<u8>,
+				encode_u8vector_argument(smt_non_membership_leaf_data),               // 	smt_non_membership_leaf_data: vector<u8>,
+				encode_u8vector_argument(smt_side_nodes),                             // 	smt_side_nodes: vector<u8>,
+			},
+		},
+	}
+}
