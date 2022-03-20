68565769fdaec3d1c92e237c4ffe218ad40565e6
yangjiefeng <yangjiefeng@gmail.com>
2022-03-20 18:28:27 +0800

    add events BCS code

---
+++ off-chain-service/events/lib.go
@@ -0,0 +1,408 @@
+package events
+
+import (
+	"fmt"
+
+	"github.com/novifinancial/serde-reflection/serde-generate/runtime/golang/bcs"
+	"github.com/novifinancial/serde-reflection/serde-generate/runtime/golang/serde"
+)
+
+type AccountAddress [16]uint8
+
+func (obj *AccountAddress) Serialize(serializer serde.Serializer) error {
+	if err := serializer.IncreaseContainerDepth(); err != nil {
+		return err
+	}
+	if err := serialize_array16_u8_array((([16]uint8)(*obj)), serializer); err != nil {
+		return err
+	}
+	serializer.DecreaseContainerDepth()
+	return nil
+}
+
+func (obj *AccountAddress) BcsSerialize() ([]byte, error) {
+	if obj == nil {
+		return nil, fmt.Errorf("Cannot serialize null object")
+	}
+	serializer := bcs.NewSerializer()
+	if err := obj.Serialize(serializer); err != nil {
+		return nil, err
+	}
+	return serializer.GetBytes(), nil
+}
+
+func DeserializeAccountAddress(deserializer serde.Deserializer) (AccountAddress, error) {
+	var obj [16]uint8
+	if err := deserializer.IncreaseContainerDepth(); err != nil {
+		return (AccountAddress)(obj), err
+	}
+	if val, err := deserialize_array16_u8_array(deserializer); err == nil {
+		obj = val
+	} else {
+		return ((AccountAddress)(obj)), err
+	}
+	deserializer.DecreaseContainerDepth()
+	return (AccountAddress)(obj), nil
+}
+
+func BcsDeserializeAccountAddress(input []byte) (AccountAddress, error) {
+	if input == nil {
+		var obj AccountAddress
+		return obj, fmt.Errorf("Cannot deserialize null array")
+	}
+	deserializer := bcs.NewDeserializer(input)
+	obj, err := DeserializeAccountAddress(deserializer)
+	if err == nil && deserializer.GetBufferOffset() < uint64(len(input)) {
+		return obj, fmt.Errorf("Some input bytes were not read")
+	}
+	return obj, err
+}
+
+type DomainNameId struct {
+	TopLevelDomain    []byte
+	SecondLevelDomain []byte
+}
+
+func (obj *DomainNameId) Serialize(serializer serde.Serializer) error {
+	if err := serializer.IncreaseContainerDepth(); err != nil {
+		return err
+	}
+	if err := serializer.SerializeBytes(obj.TopLevelDomain); err != nil {
+		return err
+	}
+	if err := serializer.SerializeBytes(obj.SecondLevelDomain); err != nil {
+		return err
+	}
+	serializer.DecreaseContainerDepth()
+	return nil
+}
+
+func (obj *DomainNameId) BcsSerialize() ([]byte, error) {
+	if obj == nil {
+		return nil, fmt.Errorf("Cannot serialize null object")
+	}
+	serializer := bcs.NewSerializer()
+	if err := obj.Serialize(serializer); err != nil {
+		return nil, err
+	}
+	return serializer.GetBytes(), nil
+}
+
+func DeserializeDomainNameId(deserializer serde.Deserializer) (DomainNameId, error) {
+	var obj DomainNameId
+	if err := deserializer.IncreaseContainerDepth(); err != nil {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeBytes(); err == nil {
+		obj.TopLevelDomain = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeBytes(); err == nil {
+		obj.SecondLevelDomain = val
+	} else {
+		return obj, err
+	}
+	deserializer.DecreaseContainerDepth()
+	return obj, nil
+}
+
+func BcsDeserializeDomainNameId(input []byte) (DomainNameId, error) {
+	if input == nil {
+		var obj DomainNameId
+		return obj, fmt.Errorf("Cannot deserialize null array")
+	}
+	deserializer := bcs.NewDeserializer(input)
+	obj, err := DeserializeDomainNameId(deserializer)
+	if err == nil && deserializer.GetBufferOffset() < uint64(len(input)) {
+		return obj, fmt.Errorf("Some input bytes were not read")
+	}
+	return obj, err
+}
+
+type DomainNameRegisterd struct {
+	DomainNameId       DomainNameId
+	Owner              AccountAddress
+	RegistrationPeriod uint64
+	UpdatedState       DomainNameState
+	UpdatedSmtRoot     []byte
+	PreviousSmtRoot    []byte
+}
+
+func (obj *DomainNameRegisterd) Serialize(serializer serde.Serializer) error {
+	if err := serializer.IncreaseContainerDepth(); err != nil {
+		return err
+	}
+	if err := obj.DomainNameId.Serialize(serializer); err != nil {
+		return err
+	}
+	if err := obj.Owner.Serialize(serializer); err != nil {
+		return err
+	}
+	if err := serializer.SerializeU64(obj.RegistrationPeriod); err != nil {
+		return err
+	}
+	if err := obj.UpdatedState.Serialize(serializer); err != nil {
+		return err
+	}
+	if err := serializer.SerializeBytes(obj.UpdatedSmtRoot); err != nil {
+		return err
+	}
+	if err := serializer.SerializeBytes(obj.PreviousSmtRoot); err != nil {
+		return err
+	}
+	serializer.DecreaseContainerDepth()
+	return nil
+}
+
+func (obj *DomainNameRegisterd) BcsSerialize() ([]byte, error) {
+	if obj == nil {
+		return nil, fmt.Errorf("Cannot serialize null object")
+	}
+	serializer := bcs.NewSerializer()
+	if err := obj.Serialize(serializer); err != nil {
+		return nil, err
+	}
+	return serializer.GetBytes(), nil
+}
+
+func DeserializeDomainNameRegisterd(deserializer serde.Deserializer) (DomainNameRegisterd, error) {
+	var obj DomainNameRegisterd
+	if err := deserializer.IncreaseContainerDepth(); err != nil {
+		return obj, err
+	}
+	if val, err := DeserializeDomainNameId(deserializer); err == nil {
+		obj.DomainNameId = val
+	} else {
+		return obj, err
+	}
+	if val, err := DeserializeAccountAddress(deserializer); err == nil {
+		obj.Owner = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeU64(); err == nil {
+		obj.RegistrationPeriod = val
+	} else {
+		return obj, err
+	}
+	if val, err := DeserializeDomainNameState(deserializer); err == nil {
+		obj.UpdatedState = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeBytes(); err == nil {
+		obj.UpdatedSmtRoot = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeBytes(); err == nil {
+		obj.PreviousSmtRoot = val
+	} else {
+		return obj, err
+	}
+	deserializer.DecreaseContainerDepth()
+	return obj, nil
+}
+
+func BcsDeserializeDomainNameRegisterd(input []byte) (DomainNameRegisterd, error) {
+	if input == nil {
+		var obj DomainNameRegisterd
+		return obj, fmt.Errorf("Cannot deserialize null array")
+	}
+	deserializer := bcs.NewDeserializer(input)
+	obj, err := DeserializeDomainNameRegisterd(deserializer)
+	if err == nil && deserializer.GetBufferOffset() < uint64(len(input)) {
+		return obj, fmt.Errorf("Some input bytes were not read")
+	}
+	return obj, err
+}
+
+type DomainNameRenewed struct {
+	DomainNameId    DomainNameId
+	Account         AccountAddress
+	RenewPeriod     uint64
+	UpdatedState    DomainNameState
+	UpdatedSmtRoot  []byte
+	PreviousSmtRoot []byte
+}
+
+func (obj *DomainNameRenewed) Serialize(serializer serde.Serializer) error {
+	if err := serializer.IncreaseContainerDepth(); err != nil {
+		return err
+	}
+	if err := obj.DomainNameId.Serialize(serializer); err != nil {
+		return err
+	}
+	if err := obj.Account.Serialize(serializer); err != nil {
+		return err
+	}
+	if err := serializer.SerializeU64(obj.RenewPeriod); err != nil {
+		return err
+	}
+	if err := obj.UpdatedState.Serialize(serializer); err != nil {
+		return err
+	}
+	if err := serializer.SerializeBytes(obj.UpdatedSmtRoot); err != nil {
+		return err
+	}
+	if err := serializer.SerializeBytes(obj.PreviousSmtRoot); err != nil {
+		return err
+	}
+	serializer.DecreaseContainerDepth()
+	return nil
+}
+
+func (obj *DomainNameRenewed) BcsSerialize() ([]byte, error) {
+	if obj == nil {
+		return nil, fmt.Errorf("Cannot serialize null object")
+	}
+	serializer := bcs.NewSerializer()
+	if err := obj.Serialize(serializer); err != nil {
+		return nil, err
+	}
+	return serializer.GetBytes(), nil
+}
+
+func DeserializeDomainNameRenewed(deserializer serde.Deserializer) (DomainNameRenewed, error) {
+	var obj DomainNameRenewed
+	if err := deserializer.IncreaseContainerDepth(); err != nil {
+		return obj, err
+	}
+	if val, err := DeserializeDomainNameId(deserializer); err == nil {
+		obj.DomainNameId = val
+	} else {
+		return obj, err
+	}
+	if val, err := DeserializeAccountAddress(deserializer); err == nil {
+		obj.Account = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeU64(); err == nil {
+		obj.RenewPeriod = val
+	} else {
+		return obj, err
+	}
+	if val, err := DeserializeDomainNameState(deserializer); err == nil {
+		obj.UpdatedState = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeBytes(); err == nil {
+		obj.UpdatedSmtRoot = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeBytes(); err == nil {
+		obj.PreviousSmtRoot = val
+	} else {
+		return obj, err
+	}
+	deserializer.DecreaseContainerDepth()
+	return obj, nil
+}
+
+func BcsDeserializeDomainNameRenewed(input []byte) (DomainNameRenewed, error) {
+	if input == nil {
+		var obj DomainNameRenewed
+		return obj, fmt.Errorf("Cannot deserialize null array")
+	}
+	deserializer := bcs.NewDeserializer(input)
+	obj, err := DeserializeDomainNameRenewed(deserializer)
+	if err == nil && deserializer.GetBufferOffset() < uint64(len(input)) {
+		return obj, fmt.Errorf("Some input bytes were not read")
+	}
+	return obj, err
+}
+
+type DomainNameState struct {
+	DomainNameId   DomainNameId
+	ExpirationDate uint64
+	Owner          AccountAddress
+}
+
+func (obj *DomainNameState) Serialize(serializer serde.Serializer) error {
+	if err := serializer.IncreaseContainerDepth(); err != nil {
+		return err
+	}
+	if err := obj.DomainNameId.Serialize(serializer); err != nil {
+		return err
+	}
+	if err := serializer.SerializeU64(obj.ExpirationDate); err != nil {
+		return err
+	}
+	if err := obj.Owner.Serialize(serializer); err != nil {
+		return err
+	}
+	serializer.DecreaseContainerDepth()
+	return nil
+}
+
+func (obj *DomainNameState) BcsSerialize() ([]byte, error) {
+	if obj == nil {
+		return nil, fmt.Errorf("Cannot serialize null object")
+	}
+	serializer := bcs.NewSerializer()
+	if err := obj.Serialize(serializer); err != nil {
+		return nil, err
+	}
+	return serializer.GetBytes(), nil
+}
+
+func DeserializeDomainNameState(deserializer serde.Deserializer) (DomainNameState, error) {
+	var obj DomainNameState
+	if err := deserializer.IncreaseContainerDepth(); err != nil {
+		return obj, err
+	}
+	if val, err := DeserializeDomainNameId(deserializer); err == nil {
+		obj.DomainNameId = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeU64(); err == nil {
+		obj.ExpirationDate = val
+	} else {
+		return obj, err
+	}
+	if val, err := DeserializeAccountAddress(deserializer); err == nil {
+		obj.Owner = val
+	} else {
+		return obj, err
+	}
+	deserializer.DecreaseContainerDepth()
+	return obj, nil
+}
+
+func BcsDeserializeDomainNameState(input []byte) (DomainNameState, error) {
+	if input == nil {
+		var obj DomainNameState
+		return obj, fmt.Errorf("Cannot deserialize null array")
+	}
+	deserializer := bcs.NewDeserializer(input)
+	obj, err := DeserializeDomainNameState(deserializer)
+	if err == nil && deserializer.GetBufferOffset() < uint64(len(input)) {
+		return obj, fmt.Errorf("Some input bytes were not read")
+	}
+	return obj, err
+}
+func serialize_array16_u8_array(value [16]uint8, serializer serde.Serializer) error {
+	for _, item := range value {
+		if err := serializer.SerializeU8(item); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func deserialize_array16_u8_array(deserializer serde.Deserializer) ([16]uint8, error) {
+	var obj [16]uint8
+	for i := range obj {
+		if val, err := deserializer.DeserializeU8(); err == nil {
+			obj[i] = val
+		} else {
+			return obj, err
+		}
+	}
+	return obj, nil
+}
---
+++ off-chain-service/transactoins/libext.go
@@ -0,0 +1,141 @@
+package transactoins
+
+import (
+	"strings"
+
+	"github.com/novifinancial/serde-reflection/serde-generate/runtime/golang/bcs"
+	"github.com/novifinancial/serde-reflection/serde-generate/runtime/golang/serde"
+	diemtypes "github.com/starcoinorg/starcoin-go/types"
+)
+
+func EncodeEmptyArgsTxPaylaod(module string, function string) diemtypes.TransactionPayload {
+	return &diemtypes.TransactionPayload__ScriptFunction{
+		diemtypes.ScriptFunction{
+			Module:   *ParseModuleId(module),
+			Function: diemtypes.Identifier(function),
+			TyArgs:   []diemtypes.TypeTag{},
+			Args:     [][]byte{},
+		},
+	}
+}
+
+func EncodeU64AndU8TxPaylaod(module string, function string, u1 uint64, u2 uint8) diemtypes.TransactionPayload {
+	return &diemtypes.TransactionPayload__ScriptFunction{
+		diemtypes.ScriptFunction{
+			Module:   *ParseModuleId(module),
+			Function: diemtypes.Identifier(function),
+			TyArgs:   []diemtypes.TypeTag{},
+			Args:     [][]byte{encode_u64_argument(u1), encode_u8_argument(u2)},
+		},
+	}
+}
+
+func EncodeTwoTypeArgsAndU128TxPaylaod(module string, function string, t1, t2 diemtypes.TypeTag, u serde.Uint128) diemtypes.TransactionPayload {
+	return &diemtypes.TransactionPayload__ScriptFunction{
+		diemtypes.ScriptFunction{
+			Module:   *ParseModuleId(module),
+			Function: diemtypes.Identifier(function),
+			TyArgs:   []diemtypes.TypeTag{t1, t2},
+			Args:     [][]byte{encode_u128_argument(u)},
+		},
+	}
+}
+
+func EncodeTwoTypeArgsAndTwoU128TxPaylaod(module string, function string, t1, t2 diemtypes.TypeTag, u1, u2 serde.Uint128) diemtypes.TransactionPayload {
+	return &diemtypes.TransactionPayload__ScriptFunction{
+		diemtypes.ScriptFunction{
+			Module:   *ParseModuleId(module),
+			Function: diemtypes.Identifier(function),
+			TyArgs:   []diemtypes.TypeTag{t1, t2},
+			Args:     [][]byte{encode_u128_argument(u1), encode_u128_argument(u2)},
+		},
+	}
+}
+
+func EncodeTwoTypeArgsAndThreeU128TxPaylaod(module string, function string, t1, t2 diemtypes.TypeTag, u1, u2, u3 serde.Uint128) diemtypes.TransactionPayload {
+	return &diemtypes.TransactionPayload__ScriptFunction{
+		diemtypes.ScriptFunction{
+			Module:   *ParseModuleId(module),
+			Function: diemtypes.Identifier(function),
+			TyArgs:   []diemtypes.TypeTag{t1, t2},
+			Args:     [][]byte{encode_u128_argument(u1), encode_u128_argument(u2), encode_u128_argument(u3)},
+		},
+	}
+}
+
+func EncodeTwoTypeArgsAndFourU128TxPaylaod(module string, function string, t1, t2 diemtypes.TypeTag, u1, u2, u3, u4 serde.Uint128) diemtypes.TransactionPayload {
+	return &diemtypes.TransactionPayload__ScriptFunction{
+		diemtypes.ScriptFunction{
+			Module:   *ParseModuleId(module),
+			Function: diemtypes.Identifier(function),
+			TyArgs:   []diemtypes.TypeTag{t1, t2},
+			Args:     [][]byte{encode_u128_argument(u1), encode_u128_argument(u2), encode_u128_argument(u3), encode_u128_argument(u4)},
+		},
+	}
+}
+
+func encode_u128_argument(arg serde.Uint128) []byte {
+
+	s := bcs.NewSerializer()
+	if err := s.SerializeU128(arg); err == nil {
+		return s.GetBytes()
+	}
+
+	panic("Unable to serialize argument of type u128")
+}
+
+func encode_u8vector_argument(arg []byte) []byte {
+
+	s := bcs.NewSerializer()
+	if err := s.SerializeBytes(arg); err == nil {
+		return s.GetBytes()
+	}
+
+	panic("Unable to serialize argument of type u8vector")
+}
+
+func encode_vecbytes_argument(arg diemtypes.VecBytes) []byte {
+
+	if val, err := arg.BcsSerialize(); err == nil {
+		{
+			return val
+		}
+	}
+
+	panic("Unable to serialize argument of type vecbytes")
+}
+
+func encode_u64_argument(arg uint64) []byte {
+
+	s := bcs.NewSerializer()
+	if err := s.SerializeU64(arg); err == nil {
+		return s.GetBytes()
+	}
+
+	panic("Unable to serialize argument of type u64")
+}
+
+func encode_u8_argument(arg uint8) []byte {
+
+	s := bcs.NewSerializer()
+	if err := s.SerializeU8(arg); err == nil {
+		return s.GetBytes()
+	}
+
+	panic("Unable to serialize argument of type u64")
+}
+
+func ParseModuleId(str string) *diemtypes.ModuleId {
+	ss := strings.Split(str, "::")
+	if len(ss) < 2 {
+		panic("module Id string format error")
+	}
+	addr, err := diemtypes.ToAccountAddress(ss[0])
+	if err != nil {
+		panic("module Id string address format error")
+	}
+	return &diemtypes.ModuleId{
+		Address: *addr,                       //[16]uint8{164, 216, 175, 70, 82, 187, 53, 191, 210, 134, 211, 71, 12, 28, 90, 61},
+		Name:    diemtypes.Identifier(ss[1]), //"CrossChainScript",
+	}
+}
