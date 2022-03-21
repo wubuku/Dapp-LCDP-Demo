e54da48c73567852851eee829d05fed1b1476659
yangjiefeng <yangjiefeng@gmail.com>
2022-03-21 11:39:06 +0800

    add BCS Serialize DomainNameId

--- off-chain-service/db/bcs.go
+++ off-chain-service/db/bcs.go
@@ -7,31 +7,6 @@ import (
 	"github.com/novifinancial/serde-reflection/serde-generate/runtime/golang/serde"
 )
 
-func (obj *DomainNameId) Serialize(serializer serde.Serializer) error {
-	if err := serializer.IncreaseContainerDepth(); err != nil {
-		return err
-	}
-	if err := serializer.SerializeBytes([]byte(obj.TopLevelDomain)); err != nil {
-		return err
-	}
-	if err := serializer.SerializeBytes([]byte(obj.SecondLevelDomain)); err != nil {
-		return err
-	}
-	serializer.DecreaseContainerDepth()
-	return nil
-}
-
-func (obj *DomainNameId) BcsSerialize() ([]byte, error) {
-	if obj == nil {
-		return nil, fmt.Errorf("Cannot serialize null object")
-	}
-	serializer := bcs.NewSerializer()
-	if err := obj.Serialize(serializer); err != nil {
-		return nil, err
-	}
-	return serializer.GetBytes(), nil
-}
-
 func (obj *DomainNameState) Serialize(serializer serde.Serializer) error {
 	if err := serializer.IncreaseContainerDepth(); err != nil {
 		return err
@@ -65,6 +40,63 @@ func (obj *DomainNameState) BcsSerialize() ([]byte, error) {
 	return serializer.GetBytes(), nil
 }
 
+func (obj *DomainNameId) Serialize(serializer serde.Serializer) error {
+	if err := serializer.IncreaseContainerDepth(); err != nil {
+		return err
+	}
+	if err := serializer.SerializeBytes([]byte(obj.TopLevelDomain)); err != nil {
+		return err
+	}
+	if err := serializer.SerializeBytes([]byte(obj.SecondLevelDomain)); err != nil {
+		return err
+	}
+	serializer.DecreaseContainerDepth()
+	return nil
+}
+
+func (obj *DomainNameId) BcsSerialize() ([]byte, error) {
+	if obj == nil {
+		return nil, fmt.Errorf("Cannot serialize null object")
+	}
+	serializer := bcs.NewSerializer()
+	if err := obj.Serialize(serializer); err != nil {
+		return nil, err
+	}
+	return serializer.GetBytes(), nil
+}
+
+func DeserializeDomainNameId(deserializer serde.Deserializer) (DomainNameId, error) {
+	var obj DomainNameId
+	if err := deserializer.IncreaseContainerDepth(); err != nil {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeBytes(); err == nil {
+		obj.TopLevelDomain = string(val)
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeBytes(); err == nil {
+		obj.SecondLevelDomain = string(val)
+	} else {
+		return obj, err
+	}
+	deserializer.DecreaseContainerDepth()
+	return obj, nil
+}
+
+func BcsDeserializeDomainNameId(input []byte) (DomainNameId, error) {
+	if input == nil {
+		var obj DomainNameId
+		return obj, fmt.Errorf("Cannot deserialize null array")
+	}
+	deserializer := bcs.NewDeserializer(input)
+	obj, err := DeserializeDomainNameId(deserializer)
+	if err == nil && deserializer.GetBufferOffset() < uint64(len(input)) {
+		return obj, fmt.Errorf("Some input bytes were not read")
+	}
+	return obj, err
+}
+
 type AccountAddress [16]uint8
 
 func (obj *AccountAddress) Serialize(serializer serde.Serializer) error {
@@ -89,6 +121,33 @@ func (obj *AccountAddress) BcsSerialize() ([]byte, error) {
 	return serializer.GetBytes(), nil
 }
 
+func DeserializeAccountAddress(deserializer serde.Deserializer) (AccountAddress, error) {
+	var obj [16]uint8
+	if err := deserializer.IncreaseContainerDepth(); err != nil {
+		return (AccountAddress)(obj), err
+	}
+	if val, err := deserialize_array16_u8_array(deserializer); err == nil {
+		obj = val
+	} else {
+		return ((AccountAddress)(obj)), err
+	}
+	deserializer.DecreaseContainerDepth()
+	return (AccountAddress)(obj), nil
+}
+
+func BcsDeserializeAccountAddress(input []byte) (AccountAddress, error) {
+	if input == nil {
+		var obj AccountAddress
+		return obj, fmt.Errorf("Cannot deserialize null array")
+	}
+	deserializer := bcs.NewDeserializer(input)
+	obj, err := DeserializeAccountAddress(deserializer)
+	if err == nil && deserializer.GetBufferOffset() < uint64(len(input)) {
+		return obj, fmt.Errorf("Some input bytes were not read")
+	}
+	return obj, err
+}
+
 func serialize_array16_u8_array(value [16]uint8, serializer serde.Serializer) error {
 	for _, item := range value {
 		if err := serializer.SerializeU8(item); err != nil {
@@ -108,4 +167,75 @@ func deserialize_array16_u8_array(deserializer serde.Deserializer) ([16]uint8, e
 		}
 	}
 	return obj, nil
+}
+
+type TokenCode struct {
+	Address AccountAddress
+	Module  string
+	Name    string
+}
+
+func (obj *TokenCode) Serialize(serializer serde.Serializer) error {
+	if err := serializer.IncreaseContainerDepth(); err != nil {
+		return err
+	}
+	if err := obj.Address.Serialize(serializer); err != nil {
+		return err
+	}
+	if err := serializer.SerializeStr(obj.Module); err != nil {
+		return err
+	}
+	if err := serializer.SerializeStr(obj.Name); err != nil {
+		return err
+	}
+	serializer.DecreaseContainerDepth()
+	return nil
+}
+
+func (obj *TokenCode) BcsSerialize() ([]byte, error) {
+	if obj == nil {
+		return nil, fmt.Errorf("Cannot serialize null object")
+	}
+	serializer := bcs.NewSerializer()
+	if err := obj.Serialize(serializer); err != nil {
+		return nil, err
+	}
+	return serializer.GetBytes(), nil
+}
+
+func DeserializeTokenCode(deserializer serde.Deserializer) (TokenCode, error) {
+	var obj TokenCode
+	if err := deserializer.IncreaseContainerDepth(); err != nil {
+		return obj, err
+	}
+	if val, err := DeserializeAccountAddress(deserializer); err == nil {
+		obj.Address = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeStr(); err == nil {
+		obj.Module = val
+	} else {
+		return obj, err
+	}
+	if val, err := deserializer.DeserializeStr(); err == nil {
+		obj.Name = val
+	} else {
+		return obj, err
+	}
+	deserializer.DecreaseContainerDepth()
+	return obj, nil
+}
+
+func BcsDeserializeTokenCode(input []byte) (TokenCode, error) {
+	if input == nil {
+		var obj TokenCode
+		return obj, fmt.Errorf("Cannot deserialize null array")
+	}
+	deserializer := bcs.NewDeserializer(input)
+	obj, err := DeserializeTokenCode(deserializer)
+	if err == nil && deserializer.GetBufferOffset() < uint64(len(input)) {
+		return obj, fmt.Errorf("Some input bytes were not read")
+	}
+	return obj, err
 }
