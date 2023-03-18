b71af665fd96832dd330b21604dc047017d21ae4
yangjiefeng <yangjiefeng@gmail.com>
2023-03-18 22:08:37 +0800

    update code comments

--- sui-java-service/suiDemoContracts-common/src/generated/java/org/dddml/suidemocontracts/specialization/IdGenerator.java
+++ sui-java-service/suiDemoContracts-common/src/generated/java/org/dddml/suidemocontracts/specialization/IdGenerator.java
@@ -1,17 +1,68 @@
 package org.dddml.suidemocontracts.specialization;
 
+/**
+ * Digest-based (hash-based) ID generator interface.
+ * Generates a digest-based ID from the command information used to create an object.
+ * Example usage:
+ * <pre>{@code
+ *     public String createWithoutId(AttributeSetInstanceCommand.CreateAttributeSetInstance c) {
+ *         String idObj = getIdGenerator().generateId(c);
+ *         AttributeSetInstanceState state = getStateRepository().get(idObj, true);
+ *         if (state != null) {
+ *             if (getIdGenerator().equals(c, state)) {
+ *                 return state.getAttributeSetInstanceId();
+ *             }
+ *
+ *             if (getIdGenerator().isArbitraryIdEnabled()) {
+ *                 idObj = getIdGenerator().getNextId();
+ *             } else {
+ *                 throw DomainError.named("instanceExist", "the instance already exist, Id: %1$s , aggreate name: %2$s ", idObj, "AttributeSetInstance");
+ *             }
+ *         }
+ *         c.setAttributeSetInstanceId(idObj);
+ *         when(c);
+ *         return idObj;
+ *
+ *     }
+ * }</pre>
+ *
+ * @param <TId>      ID type.
+ * @param <TCommand> Command type.
+ * @param <TState>   State type.
+ */
 public interface IdGenerator<TId, TCommand, TState> {
 
-    /* Generate Id form command info. */
+    /**
+     * Generate digest-based ID form command info.
+     *
+     * @param command
+     * @return Digest-based (hash-based) ID.
+     */
     TId generateId(TCommand command);
 
-    /* Get next (Arbitrary)Id. */
+    /**
+     * Generate an (arbitrary) ID.
+     *
+     * @return
+     */
     TId getNextId();
 
-    /* Would the Id generated frorm command be equals the state? */
+    /**
+     * Checks whether the existing object state is "equal" to the command used to create an object,
+     * i.e., whether the existing object was created with that command.
+     *
+     * @param command
+     * @param state
+     * @return
+     */
     boolean equals(TCommand command, TState state);
 
-    /* Is Arbitrary Id enabled. */
+    /**
+     * Determines if the generator can fall back to generating an arbitrary ID
+     * if the generated ID conflicts with the ID of a saved object.
+     *
+     * @return true if falling back is allowed.
+     */
     boolean isArbitraryIdEnabled();
 
 
