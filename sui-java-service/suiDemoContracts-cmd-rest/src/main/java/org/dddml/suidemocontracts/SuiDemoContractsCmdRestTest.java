1820cad3ce60520c8265ea56ba48db38a9fed2c2
yangjiefeng <yangjiefeng@gmail.com>
2023-03-21 15:46:45 +0800

    refactor configurations

--- sui-java-service/suiDemoContracts-cmd-rest/src/main/java/org/dddml/suidemocontracts/SuiDemoContractsCmdRestTest.java
+++
@@ -1,7 +0,0 @@
-//package org.dddml.suidemocontracts;
-//import org.dddml.suidemocontracts.domain.meta.M;
-//public class SuiDemoContractsCmdRestTest {
-//    public static void main(String[] args) {
-//        System.out.println(M.class);
-//    }
-//}
