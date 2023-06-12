328e2b8bf0d88f0c7bbc9408accd8ce68801620c
yangjiefeng <yangjiefeng@gmail.com>
2023-06-12 15:03:11 +0800

    re-generate Rooch code

--- rooch-java-service/roochDemoContracts-service-rest/src/generated/java/org/dddml/roochdemocontracts/rooch/bcs/ReferenceVO.java
+++ rooch-java-service/roochDemoContracts-service-rest/src/generated/java/org/dddml/roochdemocontracts/rooch/bcs/ReferenceVO.java
@@ -7,30 +7,30 @@ package org.dddml.roochdemocontracts.rooch.bcs;
 
 import java.util.Objects;
 
-public class Month {
+public class ReferenceVO {
 
-    public Year year;
+    public Long referenceNumber;
 
-    public Byte number;
+    public String title;
 
-    public Boolean isLeap;
+    public java.util.Optional<String> url;
 
-    public static Month deserialize(com.novi.serde.Deserializer deserializer) throws com.novi.serde.DeserializationError {
+    public static ReferenceVO deserialize(com.novi.serde.Deserializer deserializer) throws com.novi.serde.DeserializationError {
         deserializer.increase_container_depth();
-        Month value = new Month();
-        value.year = Year.deserialize(deserializer);
-        value.number = deserializer.deserialize_u8();
-        value.isLeap = deserializer.deserialize_bool();
+        ReferenceVO value = new ReferenceVO();
+        value.referenceNumber = deserializer.deserialize_u64();
+        value.title = deserializer.deserialize_str();
+        value.url = deserializer.deserialize_option_tag() ? java.util.Optional.of(deserializer.deserialize_str()): java.util.Optional.empty();
         deserializer.decrease_container_depth();
         return value;
     }
 
-    public static Month bcsDeserialize(byte[] input) throws com.novi.serde.DeserializationError {
+    public static ReferenceVO bcsDeserialize(byte[] input) throws com.novi.serde.DeserializationError {
         if (input == null) {
             throw new com.novi.serde.DeserializationError("Cannot deserialize null array");
         }
         com.novi.serde.Deserializer deserializer = new com.novi.bcs.BcsDeserializer(input);
-        Month value = deserialize(deserializer);
+        ReferenceVO value = deserialize(deserializer);
         if (deserializer.get_buffer_offset() < input.length) {
             throw new com.novi.serde.DeserializationError("Some input bytes were not read");
         }
@@ -39,9 +39,9 @@ public class Month {
 
     public void serialize(com.novi.serde.Serializer serializer) throws com.novi.serde.SerializationError {
         serializer.increase_container_depth();
-        year.serialize(serializer);
-        serializer.serialize_u8(number);
-        serializer.serialize_bool(isLeap);
+        serializer.serialize_u64(referenceNumber);
+        serializer.serialize_str(title);
+        if (url.isPresent()) { serializer.serialize_option_tag(true); serializer.serialize_str(url.get()); } else { serializer.serialize_option_tag(false); }
         serializer.decrease_container_depth();
     }
 
@@ -55,21 +55,21 @@ public class Month {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Month other = (Month) o;
-        return Objects.equals(year, other.year) && Objects.equals(number, other.number) && Objects.equals(isLeap, other.isLeap);
+        ReferenceVO other = (ReferenceVO) o;
+        return Objects.equals(referenceNumber, other.referenceNumber) && Objects.equals(title, other.title) && Objects.equals(url, other.url);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(year, number, isLeap);
+        return Objects.hash(referenceNumber, title, url);
     }
 
     @Override
     public String toString() {
-        return "Month{" +
-                "year=" + year +
-                ", number=" + number +
-                ", isLeap=" + isLeap +
+        return "ReferenceVO{" +
+                "referenceNumber=" + referenceNumber +
+                ", title=" + '\'' + title + '\'' +
+                ", url=" + url +
                 '}';
     }
     
