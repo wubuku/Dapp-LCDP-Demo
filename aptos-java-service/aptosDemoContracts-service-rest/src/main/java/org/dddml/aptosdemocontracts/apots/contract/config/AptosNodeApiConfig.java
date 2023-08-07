0276edae92f5270727c9226dc465b04b7ac1d6b4
yangjiefeng <yangjiefeng@gmail.com>
2023-08-07 14:51:25 +0800

    regenerate code

---
+++ aptos-java-service/aptosDemoContracts-service-rest/src/main/java/org/dddml/aptosdemocontracts/apots/contract/config/AptosNodeApiConfig.java
@@ -0,0 +1,28 @@
+package org.dddml.aptosdemocontracts.apots.contract.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.github.wubuku.aptos.utils.NodeApiClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.MalformedURLException;
+
+@Configuration
+public class AptosNodeApiConfig {
+    @Value("${aptos.contract.node-api.base-url}")
+    private String nodeApiBaseUrl;
+
+    @Bean
+    public ObjectMapper aptosNodeApiObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        return objectMapper;
+    }
+
+    @Bean
+    public NodeApiClient aptosNodeApiClient() throws MalformedURLException {
+        return new NodeApiClient(nodeApiBaseUrl, aptosNodeApiObjectMapper());
+    }
+}
