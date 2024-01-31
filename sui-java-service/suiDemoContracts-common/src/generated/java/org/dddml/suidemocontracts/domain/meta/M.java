72692f60f0359039b26b38f62a83dd88fcf3e3bc
yangjiefeng <yangjiefeng@gmail.com>
2024-01-31 12:23:43 +0800

    update sui types

--- sui-java-service/suiDemoContracts-common/src/generated/java/org/dddml/suidemocontracts/domain/meta/M.java
+++ sui-java-service/suiDemoContracts-common/src/generated/java/org/dddml/suidemocontracts/domain/meta/M.java
@@ -122,6 +122,7 @@ public class M {
         clsMap.put("ID", String.class);
         clsMap.put("address", String.class);
         clsMap.put("Balance", java.math.BigInteger.class);
+        clsMap.put("Coin", Object.class);
         CLASS_MAP = clsMap;
     }
 
