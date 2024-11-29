6454d14db4776216247d8c5b684eec9770684b95
yangjiefeng <yangjiefeng@gmail.com>
2024-11-29 19:40:43 +0800

    update Aptos Java demo code

--- aptos-java-service/aptosDemoContracts-service/src/main/java/org/dddml/aptosdemocontracts/config/DatabaseConfig.java
+++ aptos-java-service/aptosDemoContracts-service/src/main/java/org/dddml/aptosdemocontracts/config/DatabaseConfig.java
@@ -2,9 +2,13 @@ package org.dddml.aptosdemocontracts.config;
 
 import org.dddml.aptosdemocontracts.specialization.NullReadOnlyProxyGenerator;
 import org.dddml.aptosdemocontracts.specialization.ReadOnlyProxyGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
@@ -12,24 +16,33 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.util.Properties;
 
 // Enables Spring's annotation-driven transaction management capability, similar to the support found in Spring's <tx:*> XML namespace.
 @EnableTransactionManagement
 @Configuration
 public class DatabaseConfig {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ResourceLoader resourceLoader;
 
     @Autowired
     private DataSource dataSource;
 
+    // 注入 Spring 配置
+    @Autowired
+    private Environment springEnvironment;
 
-//    @Bean
-//    public JpaTransactionManager transactionManager() throws IOException {
-//        JpaTransactionManager transactionManager = new JpaTransactionManager();
-//        transactionManager.setEntityManagerFactory(hibernateSessionFactory().getObject());
-//        return transactionManager;
-//    }
+    @Value("${spring.jpa.properties.hibernate.dialect}")
+    private String hibernateDialect;
+
+    // @Bean
+    // public JpaTransactionManager transactionManager() throws IOException {
+    // JpaTransactionManager transactionManager = new JpaTransactionManager();
+    // transactionManager.setEntityManagerFactory(hibernateSessionFactory().getObject());
+    // return transactionManager;
+    // }
 
     @Bean(name = {
             "hibernateSessionFactory",
@@ -42,14 +55,30 @@ public class DatabaseConfig {
                 ResourcePatternUtils.getResourcePatternResolver(resourceLoader)
                         .getResources("classpath:/hibernate/*.hbm.xml")
         );
-        sessionFactory.setPackagesToScan("org.dddml.aptosdemocontracts.*"); //add annotation mappings
+        sessionFactory.setPackagesToScan("org.dddml.aptosdemocontracts.*"); // add annotation mappings
 
-        //hibernate.physical_naming_strategy=org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy
-        sessionFactory.setPhysicalNamingStrategy(new org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy());
+        // hibernate.physical_naming_strategy=org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy
+        sessionFactory.setPhysicalNamingStrategy(
+                new org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy());
 
-        sessionFactory.setHibernateProperties(
-                org.hibernate.cfg.Environment.getProperties() //resources/hibernate.properties
-        );
+        // Read config from application-test.yml
+        Properties hibernateProperties = new Properties();
+
+        hibernateProperties.setProperty("hibernate.dialect", hibernateDialect);
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto",
+                springEnvironment.getProperty("spring.jpa.properties.hibernate.hbm2ddl.auto"));
+        hibernateProperties.setProperty("hibernate.show_sql",
+                springEnvironment.getProperty("spring.jpa.show-sql", "true"));
+        hibernateProperties.setProperty("hibernate.connection.pool_size",
+                springEnvironment.getProperty("spring.jpa.properties.hibernate.connection.pool_size", "1"));
+        hibernateProperties.setProperty("hibernate.cache.provider_class",
+                springEnvironment.getProperty("spring.jpa.properties.hibernate.cache.provider_class"));
+        hibernateProperties.setProperty("hibernate.cache.use_second_level_cache",
+                springEnvironment.getProperty("spring.jpa.properties.hibernate.cache.use_second_level_cache"));
+
+        log.debug("Hibernate properties: {}", hibernateProperties);
+        sessionFactory.setHibernateProperties(hibernateProperties);
+
         return sessionFactory;
     }
 
@@ -57,5 +86,4 @@ public class DatabaseConfig {
     public ReadOnlyProxyGenerator stateReadOnlyProxyGenerator() {
         return new NullReadOnlyProxyGenerator();
     }
-
 }
